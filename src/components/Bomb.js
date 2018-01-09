f325e3ab1a326451a080f05019be0a08794a9f89
Christofer Johnson <christofer.d.johnson@gmail.com>
2018-01-09 14:02:50 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -9,6 +9,12 @@ class Bomb React.component{
       secondsLeft: props.initialCount
     }
   }
+
+  render(){
+    return(
+      const timer = this.state.secondsLeft === 0 ? 'Boom' : '${this.state.secondsLeft} seconds left before I go boom!'
+        <p>{timer}</p>
+    )}
 }
 
 extends default Bomb
