3b10ace4aefc242bdeda19a33f304a47d2630b47
Christofer Johnson <christofer.d.johnson@gmail.com>
2018-01-09 13:58:15 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,14 @@
 // Bomb Component Code Goes Here
+import React from 'react'
+
+class Bomb React.component{
+  constructor(initialCount){
+    super()
+
+    this.state = {
+      secondsLeft: initialCount
+    }
+  }
+}
+
+extends default Bomb
